31d1a93c02ea8b2df9bd83f38d99cba55c1accce
hamish clark <hamish.bw.clark@gmail.com>
2026-06-09 00:34:27 +1200

    Changed default notification click behaviour to open on the correct page

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://hamish-clark.github.io/Floss-live/sw.js', { scope: 'https://hamish-clark.github.io/Floss-live/' })})}
